c6cb65aa80df007ea3b68c34b8108f9d3cbfe641
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-10-26 12:05:17 +0530

    spring boot basics

--- src/main/java/com/merc/demo/model/Employee.java
+++ src/main/java/com/merc/demo/model/Employee.java
@@ -1,8 +1,14 @@
 package com.merc.demo.model;
 
-//@Model
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employee_table")
 public class Employee {
 
+	@Id
 	private Integer eid;
 	private String firstName;
 	private Double salary;
@@ -48,3 +54,53 @@ public class Employee {
 	}
 
 }
+
+//package com.merc.demo.model;
+//
+//public class Employee {
+//
+//	private Integer eid;
+//	private String firstName;
+//	private Double salary;
+//
+//	public Employee() {
+//		super();
+//	}
+//
+//	public Employee(Integer eid, String firstName, Double salary) {
+//		super();
+//		this.eid = eid;
+//		this.firstName = firstName;
+//		this.salary = salary;
+//	}
+//
+//	public Integer getEid() {
+//		return eid;
+//	}
+//
+//	public void setEid(Integer eid) {
+//		this.eid = eid;
+//	}
+//
+//	public String getFirstName() {
+//		return firstName;
+//	}
+//
+//	public void setFirstName(String firstName) {
+//		this.firstName = firstName;
+//	}
+//
+//	public Double getSalary() {
+//		return salary;
+//	}
+//
+//	public void setSalary(Double salary) {
+//		this.salary = salary;
+//	}
+//
+//	@Override
+//	public String toString() {
+//		return "Employee [eid=" + eid + ", firstName=" + firstName + ", salary=" + salary + "]";
+//	}
+//
+//}
