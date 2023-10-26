d803a91ac31347418e7921fcdaaca2cd98983327
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-10-26 11:56:58 +0530

    spring boot basics

--- src/main/java/com/merc/demo/repository/EmployeeRepository.java
+++ src/main/java/com/merc/demo/repository/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.merc.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.merc.demo.model.Employee;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 	// code here as needed
