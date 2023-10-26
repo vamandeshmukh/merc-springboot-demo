b2cd31cbd6ffbe1bb7912a76b687243e23a6d665
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-10-26 09:54:33 +0530

    spring boot basics

--- src/main/java/com/merc/demo/controller/EmployeeController.java
+++ src/main/java/com/merc/demo/controller/EmployeeController.java
@@ -3,29 +3,69 @@ package com.merc.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.merc.demo.model.Employee;
 import com.merc.demo.service.EmployeeService;
 
 @RestController
+@RequestMapping("api")
+//@CrossOrigin(origins = "3000")
+//@CrossOrigin(origins = { "3000", "4200" })
+@CrossOrigin(origins = "*")
 public class EmployeeController {
-
-//	EmployeeService empService = new EmployeeService(); // not needed 
 
 	@Autowired
 	EmployeeService empService;
+
+//	http://localhost:8090/api/get-all-emps
 
 	@GetMapping("get-all-emps")
 	public List<Employee> getAllEmps() {
 		return empService.getAllEmployees();
 	}
 
-	@GetMapping("get-emp-by-id")
-	public Employee getEmpById() {
-		int eid = 101;
+	@GetMapping("get-emp-by-id/{empid}")
+	public Employee getEmpById(@PathVariable(name = "empid") Integer eid) {
 		return empService.getEmpById(eid);
 	}
+	
+	
 
 }
+
+//package com.merc.demo.controller;
+//
+//import java.util.List;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import com.merc.demo.model.Employee;
+//import com.merc.demo.service.EmployeeService;
+//
+//@RestController
+//public class EmployeeController {
+//
+////	EmployeeService empService = new EmployeeService(); // not needed 
+//
+//	@Autowired
+//	EmployeeService empService;
+//
+//	@GetMapping("get-all-emps")
+//	public List<Employee> getAllEmps() {
+//		return empService.getAllEmployees();
+//	}
+//
+//	@GetMapping("get-emp-by-id")
+//	public Employee getEmpById() {
+//		int eid = 101;
+//		return empService.getEmpById(eid);
+//	}
+//
+//}
--- src/main/java/com/merc/demo/service/EmployeeService.java
+++ src/main/java/com/merc/demo/service/EmployeeService.java
@@ -28,15 +28,11 @@ public class EmployeeService {
 		LOG.info("getAllEmployees");
 		LOG.warn("some warning");
 		LOG.error("some error");
-		LOG.debug("debug log");
 		return empList;
 	}
 
 	public Employee getEmpById(Integer eid) {
 		LOG.info(eid.toString());
-		LOG.warn("some warning");
-		LOG.error("some error");
-		LOG.debug("debug log");
 		for (Employee e : empList) {
 			if (e.getEid() == eid)
 				return e;
