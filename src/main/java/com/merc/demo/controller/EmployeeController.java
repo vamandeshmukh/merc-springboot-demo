dca588f6f464eb01624f71307d18e3079f16ed59
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-10-26 10:30:18 +0530

    spring boot basics

--- src/main/java/com/merc/demo/controller/EmployeeController.java
+++ src/main/java/com/merc/demo/controller/EmployeeController.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,22 +34,88 @@ public class EmployeeController {
 //	http://localhost:8090/api/get-all-emps
 
 	@GetMapping("get-all-emps")
-	public List<Employee> getAllEmps() {
-		return empService.getAllEmployees();
+	public ResponseEntity<List<Employee>> getAllEmps() {
+		List<Employee> empList = empService.getAllEmployees();
+		HttpStatus status = HttpStatus.OK;
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("message", "Employees found successfully.");
+		ResponseEntity<List<Employee>> response = new ResponseEntity<List<Employee>>(empList, headers, status);
+		LOG.info(Integer.toString(empList.size()));
+		return response;
 	}
 
 	@GetMapping("get-emp-by-id/{empid}")
-	public Employee getEmpById(@PathVariable(name = "empid") Integer eid) {
-		return empService.getEmpById(eid);
+	public ResponseEntity<Employee> getEmpById(@PathVariable(name = "empid") Integer eid) {
+		Employee empObj = empService.getEmpById(eid);
+		HttpStatus status = HttpStatus.OK;
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("message", "Employee found successfully.");
+		ResponseEntity<Employee> response = new ResponseEntity<Employee>(empObj, headers, status);
+		LOG.info(empObj.toString());
+		return response;
 	}
 
 	@PostMapping("add-emp")
-	public Employee addEmp(@RequestBody Employee emp) {
-		LOG.info(emp.toString());
-		return empService.addEmployee(emp);
+	public ResponseEntity<Employee> addEmp(@RequestBody Employee emp) {
+		Employee empObj = empService.addEmployee(emp);
+		HttpStatus status = HttpStatus.CREATED;
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("message", "Employee created successfully.");
+		ResponseEntity<Employee> response = new ResponseEntity<Employee>(empObj, headers, status);
+		LOG.info(empObj.toString());
+		return response;
 	}
 
 }
+//package com.merc.demo.controller;
+//
+//import java.util.List;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import com.merc.demo.model.Employee;
+//import com.merc.demo.service.EmployeeService;
+//
+//@RestController
+//@RequestMapping("api")
+////@CrossOrigin(origins = "3000")
+////@CrossOrigin(origins = { "3000", "4200" })
+//@CrossOrigin(origins = "*")
+//public class EmployeeController {
+//
+//	private final Logger LOG = LoggerFactory.getLogger(this.getClass());
+//
+//	@Autowired
+//	EmployeeService empService;
+//
+////	http://localhost:8090/api/get-all-emps
+//
+//	@GetMapping("get-all-emps")
+//	public List<Employee> getAllEmps() {
+//		return empService.getAllEmployees();
+//	}
+//
+//	@GetMapping("get-emp-by-id/{empid}")
+//	public Employee getEmpById(@PathVariable(name = "empid") Integer eid) {
+//		return empService.getEmpById(eid);
+//	}
+//
+//	@PostMapping("add-emp")
+//	public Employee addEmp(@RequestBody Employee emp) {
+//		LOG.info(emp.toString());
+//		return empService.addEmployee(emp);
+//	}
+//
+//}
 
 //package com.merc.demo.controller;
 //
