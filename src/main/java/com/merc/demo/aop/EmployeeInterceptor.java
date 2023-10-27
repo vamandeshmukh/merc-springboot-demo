2375f38a4dc5deae3cdea6eb2a4015475d07e9b9
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-10-27 12:06:19 +0530

    spring boot

--- src/main/java/com/merc/demo/aop/EmployeeInterceptor.java
+++ src/main/java/com/merc/demo/aop/EmployeeInterceptor.java
@@ -1,5 +1,8 @@
 package com.merc.demo.aop;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -17,5 +20,21 @@ public class EmployeeInterceptor {
 	public void serviceLog() {
 		LOG.info("A method from a service class invoked.");
 	}
+
+	@Before("execution(* com.merc.demo.service.*.*(..))")
+	public void serviceLogWithData(JoinPoint obj) {
+		LOG.info(obj.getSignature().toString());
+	}
+
+	@AfterThrowing("execution(* com.merc.demo.service.*.*(..))")
+	public void exceptionThrown(JoinPoint obj) {
+		for (Object arg : obj.getArgs())
+			LOG.info(arg.toString());
+	}
+
+	// @After("execution(* com.merc.demo.service.*.*(..))")
+//	public void serviceLog2() {
+//		LOG.info("After a method from a service class invoked.");
+//	}
 
 }
