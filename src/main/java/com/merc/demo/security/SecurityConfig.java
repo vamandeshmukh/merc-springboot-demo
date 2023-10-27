95a83fefea6fd01ca10f74ca7925c02fbc3dd5c2
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-10-27 09:52:09 +0530

    spring boot

--- src/main/java/com/merc/demo/security/SecurityConfig.java
+++ src/main/java/com/merc/demo/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.merc.demo.security;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -18,18 +20,26 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity
 public class SecurityConfig {
 
+	private final Logger LOG = LoggerFactory.getLogger(this.getClass());
+
 	@Bean
 	public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+		LOG.info(encoder.toString());
+//		UserDetails admin = User.withUsername("admin").password("pass").roles("ADMIN", "USER").build();
+//		UserDetails user = User.withUsername("user").password("pass").roles("USER").build();
 
-		UserDetails admin = User.withUsername("admin").password("pass").roles("ADMIN", "USER").build();
-		UserDetails user = User.withUsername("user").password("pass").roles("USER").build();
+		// error -
+		// Encoded password does not look like BCrypt
+		
+		UserDetails admin = User.withUsername("admin").password(encoder.encode("pass")).roles("ADMIN", "USER").build();
+		UserDetails user = User.withUsername("user").password(encoder.encode("pass")).roles("USER").build();
 
 		return new InMemoryUserDetailsManager(admin, user);
 	}
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
+		LOG.info(http.toString());
 //		return http.build();
 
 		return http.csrf().disable()
@@ -49,6 +59,7 @@ public class SecurityConfig {
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
+		LOG.info("passwordEncoder");
 		return new BCryptPasswordEncoder();
 	}
 
