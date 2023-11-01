997762bc034431af7b328286e626ca56bf76688f
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-11-01 17:47:46 +0530

    next-commit

--- src/test/java/com/merc/demo/AppTests.java
+++ src/test/java/com/merc/demo/AppTests.java
@@ -1,5 +1,7 @@
 package com.merc.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,7 +11,10 @@ class AppTests {
 //	@Test
 //	void contextLoads() {
 //	}
-	
-	
+
+	@Test
+	void testTest() {
+		assertEquals(10, 5 + 5);
+	}
 
 }
