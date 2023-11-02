57a2ddbbf1d2a3fa6b15d07fdd3cf558782f74d2
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-11-02 16:23:53 +0530

    commit n

--- src/test/java/com/merc/demo/AppTests.java
+++ src/test/java/com/merc/demo/AppTests.java
@@ -9,16 +9,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 class AppTests {
 
-//	@Test
-//	void testTest() {
-//		Assertions.assertEquals(10, 5 + 5);
-//	}
-//
-//	@Test
-//	void testTestN() {
-//		Assertions.assertNotEquals(10, 5 + 6);
-//	}
+	@Test
+	void testTest() {
+		Assertions.assertEquals(10, 5 + 5);
+	}
 
+	@Test
+	void testTestN() {
+		Assertions.assertNotEquals(10, 5 + 6);
+	}
 }
 
 // testAddEmployee
