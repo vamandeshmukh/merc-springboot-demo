ae4596888e65bad9a083f1f32c8d825b5dc40f21
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-11-02 15:30:29 +0530

    commit n

--- src/test/java/com/merc/demo/AppTests.java
+++ src/test/java/com/merc/demo/AppTests.java
@@ -9,24 +9,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AppTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
 
-//	@Test
-//	void testTest() {
-//		assertEquals(10, 5 + 5);
-//	}
-//
-//	@Test
-//	void testTest2() {
-//		assertEquals(10, 5 + 5);
-//	}
+	@Test
+	void testTest() {
+		assertEquals(10, 5 + 5);
+	}
 
-//	@Test
-//	void testTestN() {
-//		assertNotEquals(10, 5 + 6);
-//	}
+	@Test
+	void testTestN() {
+		assertNotEquals(10, 5 + 6);
+	}
 }
 
 // testAddEmployee
