8e11e9bdc61199e32c7944898a7fb0598b7d71ba
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-11-02 15:46:10 +0530

    commit n

--- src/test/java/com/merc/demo/AppTests.java
+++ src/test/java/com/merc/demo/AppTests.java
@@ -1,21 +1,17 @@
 package com.merc.demo;
 
 import org.junit.jupiter.api.Assertions;
-
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AppTests {
 
-	@Disabled
 	@Test
 	void testTest() {
 		Assertions.assertEquals(10, 5 + 5);
 	}
 
-	@Disabled
 	@Test
 	void testTestN() {
 		Assertions.assertNotEquals(10, 5 + 6);
