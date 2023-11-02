0e5fe4cb35b49c0243bbc07c7f7c4e22d6c60beb
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-11-02 07:18:49 +0530

    next-commit

--- src/test/java/com/merc/demo/AppTests.java
+++ src/test/java/com/merc/demo/AppTests.java
@@ -9,10 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AppTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
-
 	@Test
 	void testTest() {
 		assertEquals(10, 5 + 5);
